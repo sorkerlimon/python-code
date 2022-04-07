b225b786656971a59b2223f93995ccb751167575
sorker limon <sorkerlimon18@gmail.com>
2022-04-08 04:43:23 +0600

    first commit

---
+++ datastructure.py
@@ -0,0 +1,59 @@
+#############list start
+
+# letters = ["a","b","c"]
+# print(letters)
+# matrix = [[1,2],[3,4]]
+# print(matrix)
+#
+# zeros = [0] * 10
+# print(zeros)
+# zeros = 0 * 10
+# print(zeros)
+#
+# zeros = [0] * 5
+# combined = zeros + letters
+# print(combined)
+#
+#
+# number = list(range(20))
+# print(number)
+#
+#
+# chars = list("hello world")
+# print(chars)
+# print(len(chars))
+
+
+# letters = ["r","g","v"]
+# print(letters)
+# matrix = [[1,2],[3,4]]
+# print(matrix)
+#
+# zeros = [1] * 5
+# print(
+#     zeros
+# )
+#
+#
+# combined = letters + zeros
+# print(combined)
+# print(len(combined))
+#
+# number = list(range(10))
+# print(number)
+
+######### List item acces
+
+# letters = ["r","g","v","f","g","g"]
+# print(letters[0:2])
+# letters[1]="B"
+# print(letters)
+# print(letters[::2])
+#
+# number = list(range(20))
+# print(number[::2])
+
+
+######
+
+
