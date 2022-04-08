0e0be84e2f1eeec6ffa4f5e0380b986ceee4ea40
sorker limon <sorkerlimon18@gmail.com>
2022-04-09 04:31:22 +0600

    data stucture code practice

--- datastructure.py
+++ datastructure.py
@@ -54,6 +54,44 @@
 # print(number[::2])
 
 
-######
+###### unpacking method
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# first, second,*all ,last= numbers
+#
+# print(first)
+# print(second)
+# print(all)
+# print(last)
+
+## Looping over the lisit
+
+# letters = ["a","b","c"]
+
+# letter = [letter for letter in enumerate(letters)]
+# print(letter)
+
+# for letter in enumerate(letters):
+#     print(letter)
+#
+# for index ,letter in enumerate(letters):
+#     print(index,letter)
+#
+# for index, letter in enumerate(letters):
+#     print(f"Index number is :{index} & {letter} this is the letter")
+
+####### list  adding and remove
+
+letters = ["a","b","c"]
+letters.append("d")
+print(letters)
+letters.insert(0,"Limon")
+print(letters)
+letters.pop()
+print(letters)
+letters.pop(1)
+print(letters)
+letters.remove("b")
+print(letters)
 
 
