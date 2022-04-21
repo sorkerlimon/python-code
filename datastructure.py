500705f8d83839214c72f02f5f8c68b637a1be1e
sorker limon <sorkerlimon18@gmail.com>
2022-04-22 04:23:20 +0600

    Data structure code added

--- datastructure.py
+++ datastructure.py
@@ -490,3 +490,25 @@ items_list = [
 #
 # combined = {**first,**second,"z":1}
 # print(combined)
+
+############ Exercise
+
+
+########## Most repeted character in this sentence
+from pprint import  pprint
+#
+# sentence = "This is my favourite sentece ssss"
+#
+# char_frequency = {}
+# for char in sentence:
+#     if char in char_frequency:
+#         char_frequency[char] += 1
+#     else:
+#         char_frequency[char] = 1
+#
+# # pprint(char_frequency,width=1)
+# char_frequency_sorted = sorted(char_frequency.items(),key=lambda kv:kv[1],reverse=True)
+# print(char_frequency_sorted[0])
+
+
+
