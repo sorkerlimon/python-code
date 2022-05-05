71fa4153d9a176a36f7d215d34b7d2af5cb20f00
sorker limon <sorkerlimon18@gmail.com>
2022-05-05 23:49:23 +0600

    datastucture code added

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,42 @@
-try:
-    age = int(input("Age : "))
-    print(age)
-except ValueError as ex:
-    print("Type valid data")
-    print(ex)
-    print(type(ex))
-else:
-    print("No exception thrown")
-print("Execution Continuoues >......!!!")
+# try:
+#     age = int(input("Age : "))
+#     print(age)
+# except ValueError as ex:
+#     print("Type valid data")
+#     print(ex)
+#     print(type(ex))
+# else:
+#     print("No exception thrown")
+# print("Execution Continuoues >......!!!")
+
+
+
+######## Zero division error
+
+# try:
+#     age = int(input("enter ur age"))
+#     xfactor = 10 /age
+# except ZeroDivisionError as zero:
+#     print("You can not divided age ")
+# else:
+#     print(xfactor)
+
+
+
+
+
+########## Cleaning Up code file open and close
+
+# try:
+#     file = open("app.py")
+#     age = int(input("enter ur age"))
+#     xfactor = 10 /age
+# except (ValueError,ZeroDivisionError) as ex:
+#     print(ex)
+# else:
+#     print(xfactor)
+#
+# finally:
+#     file.close()
+
+
