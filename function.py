3b64b497630468e42ab407b4a0aafb3aa5324a6a
sorker limon <sorkerlimon18@gmail.com>
2022-05-07 23:55:47 +0600

    function practice first day

---
+++ function.py
@@ -0,0 +1,55 @@
+##### Function  start
+
+# def greet():
+#     print("Hi there")
+#     print("Hi there... I am limon")
+#     print("Hi there & I am sorker ")
+#
+# greet()
+
+
+
+################# function with formate account
+# def greet(name):
+#     print(f"Hi there {name}")
+#     print(f"Hi there... I am limon {name}")
+#     print(f"Hi there & I am sorker {name}")
+#
+# greet("Bangladehs")
+
+
+
+########### function with arg
+
+# def greet(first_name,last_name):
+#     print(f"Hi there {first_name} {last_name}")
+#     print(f"Hi there... {first_name} {last_name}")
+#     print(f"Hi there & I am {first_name} {last_name}")
+# try:
+#     greet("Bangladehs", "lizel")
+# except TypeError as tp:
+#     print(tp ,"Missing 1 argument")
+# else:
+#     print("Keep Going......................")
+
+
+
+######### Function 2 type 1(Perform a task ) 2(return a value)
+
+# def get_greet(name):
+#     return f"hello {name}"
+#
+# messege = get_greet("Limon")
+# print(messege)
+#
+# file = open("content.txt","w")
+# file.write(messege)
+# print("messege write done")
+#
+# with open("C:/Users/Sorker Limon/Desktop/cont.txt") as f:
+#     contents = f.read()
+#     print(contents)
+
+
+
+
